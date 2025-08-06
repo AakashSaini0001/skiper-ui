fcca5f8db8522adbf8586a5c3502ef7e03c37c34
Aakash Saini <aakashsaini0104@gmail.com>
2025-08-07 02:19:53 +0530

    changed the eslint type here

--- src/components/landingPage/HeroContent.tsx
+++ src/components/landingPage/HeroContent.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import WrapButton from "../ui/wrap-button";
 import { Globe } from "lucide-react";
+import Image from 'next/image'
+
 
 const HeroContent = () => {
   return (
     <>
     <div className="flex flex-col items-center py-16">
-      <img
+      <Image
         className="h-10 mb-14"
         src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=806890&theme=light&t=1737565356562"
         alt=""
@@ -39,8 +41,8 @@ const HeroContent = () => {
       </p>
     </div>
 
-    <img className="absolute left-24 top-80 h-[700px]" src="https://skiper-ui.com/bg/bg_1.svg" alt="" />
-    <img className="absolute right-24 top-80 h-[500px]" src="https://skiper-ui.com/bg/bg_2.svg" alt="" />
+    <Image className="absolute left-24 top-80 h-[700px]" src="https://skiper-ui.com/bg/bg_1.svg" alt="" />
+    <Image className="absolute right-24 top-80 h-[500px]" src="https://skiper-ui.com/bg/bg_2.svg" alt="" />
     </>
   );
 };
